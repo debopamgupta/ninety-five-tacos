97da5113fdf2f7eff2408d04747f643ccaaba7c2
DG <51162936+debopamgupta@users.noreply.github.com>
2022-03-24 22:23:59 +0530

    :sparkles: Implemented Random Generator

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
+import mnemonicWords from "mnemonic-words";
+
 import "./style.css";
+import "./skeleton.css";
+import "./normalize.css";
+
+const getRandomAcronym = (): string => {
+  const wordsStartingWithN = mnemonicWords.filter((word) => word.startsWith("n"));
+  const wordsStartingWithF = mnemonicWords.filter((word) => word.startsWith("f"));
+  const wordsStartingWithT = mnemonicWords.filter((word) => word.startsWith("t"));
+
+  const letterN = wordsStartingWithN[Math.floor(Math.random() * wordsStartingWithN.length)];
+  const letterF = wordsStartingWithF[Math.floor(Math.random() * wordsStartingWithN.length)];
+  const letterT = wordsStartingWithT[Math.floor(Math.random() * wordsStartingWithN.length)];
+
+  return letterN + " " + letterF + " " + letterT;
+};
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const genButton = document.querySelector<HTMLButtonElement>("button")!;
 
-app.innerHTML = `
-  <h1>NinetyFiveTacos!</h1>
-`;
+genButton!.onclick = () => {
+  app.innerText = getRandomAcronym();
+};
+
+app.innerText = getRandomAcronym();
