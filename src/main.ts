990830ae96d386b0fb4f6974d4bbe4f3c9c4172b
DG <51162936+debopamgupta@users.noreply.github.com>
2022-04-01 15:13:47 +0530

    Dont filter everytime

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,14 @@ import "./style.css";
 import "./skeleton.css";
 import "./normalize.css";
 
-const getRandomAcronym = (): string => {
-  const wordsStartingWithN = mnemonicWords.filter((word) => word.startsWith("n"));
-  const wordsStartingWithF = mnemonicWords.filter((word) => word.startsWith("f"));
-  const wordsStartingWithT = mnemonicWords.filter((word) => word.startsWith("t"));
+const wordsStartingWithN = mnemonicWords.filter((word: string) => word.startsWith("n"));
+const wordsStartingWithF = mnemonicWords.filter((word: string) => word.startsWith("f"));
+const wordsStartingWithT = mnemonicWords.filter((word: string) => word.startsWith("t"));
 
+const getRandomAcronym = (): string => {
   const letterN = wordsStartingWithN[Math.floor(Math.random() * wordsStartingWithN.length)];
   const letterF = wordsStartingWithF[Math.floor(Math.random() * wordsStartingWithN.length)];
   const letterT = wordsStartingWithT[Math.floor(Math.random() * wordsStartingWithN.length)];
-
   return letterN + " " + letterF + " " + letterT;
 };
 
